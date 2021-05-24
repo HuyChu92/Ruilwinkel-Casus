82cbb6dd918fb90cf57fc9e46173c4e7c87690a5
LorenzoClermonts <lorenzoclermonts@ziggo.nl>
2021-05-24 15:41:42 +0200

    gridview style aanpassing

--- Ruilwinkel/Categorieën.aspx.designer.cs
+++ Ruilwinkel/Categorieën.aspx.designer.cs
@@ -42,6 +42,15 @@ namespace Ruilwinkel
         protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource2;
 
         /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
         /// TextBoxCategorieNaam control.
         /// </summary>
         /// <remarks>
