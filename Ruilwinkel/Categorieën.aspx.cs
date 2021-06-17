66394767e37e6f6013c42525531ee319fa4a10a6
Huy Chu <1135546chu@zuyd.nl>
2021-06-18 00:05:27 +0200

    Verwijder functionaliteiten werken

--- Ruilwinkel/Categorieën.aspx.cs
+++ Ruilwinkel/Categorieën.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace Ruilwinkel
 {
@@ -26,13 +27,62 @@ namespace Ruilwinkel
 
         protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
         {
-            string selectedCategory = GridView2.SelectedRow.Cells[2].Text;
-            string caterogyCheck = "SELECT CATEGORY.CATEGORYNAME";
+            string selectedCategory = GridView2.SelectedRow.Cells[1].Text;
+            string caterogyCheck = "SELECT PRODUCT.PRODUCTNAME FROM PRODUCT WHERE PRODUCT.CATEGORYID = (" + selectedCategory + ")";
+            List<string> producten = new List<string>();
+            Label3.Text = selectedCategory;
+
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=productbeheerserver.database.windows.net;Initial Catalog=RuilwinkelDB;Persist Security Info=True;User ID=DevOps;Password=Zuyd2021";
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = caterogyCheck;
+            cmd.Connection = con;
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                string productNaam = dr.GetValue(0).ToString();
+                producten.Add(productNaam);
+            }
+            con.Close();
+
+            if (producten.Count == 0)
+            {
+                string deleteString = "DELETE FROM CATEGORY WHERE ID = (" + selectedCategory + ")";
+                deleteCategory(deleteString);
+                Response.Redirect("Categorieën.aspx");
+            }
+            else {
+                Messagebox(producten);
+            }
+            
         }
 
         protected void GridView2_SelectedIndexChanged(object sender, EventArgs e)
         {
-            Label3.Text = GridView2.SelectedRow.Cells[2].Text;
+            
+        }
+
+        private void deleteCategory(string command) {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=productbeheerserver.database.windows.net;Initial Catalog=RuilwinkelDB;Persist Security Info=True;User ID=DevOps;Password=Zuyd2021";
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = command;
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public void Messagebox(List<string> Message)
+        {
+            string test = "Verwijder volgende producten eerst:  ";
+            foreach (string product in Message) {
+                test += product.ToString() + ",  ";  
+            }
+            Response.Write("<script>alert('" + test + "')</script>");
         }
     }
 }
