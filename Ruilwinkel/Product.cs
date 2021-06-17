7f64df1974565781a736569c506fca232e6eb31e
LorenzoClermonts <lorenzoclermonts@ziggo.nl>
2021-06-17 15:08:27 +0200

    api get categories

---
+++ Ruilwinkel/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ruilwinkel
+{
+    public class Product
+    {
+        public int CategorieID;
+
+        public string ProductName;
+
+        public string Description;
+    }
+}
