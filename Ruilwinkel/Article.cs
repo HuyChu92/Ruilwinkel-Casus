d7c6707c589706f6d55381d82063c9ddf5d40030
Huy Chu <1135546chu@zuyd.nl>
2021-06-18 17:56:39 +0200

    Article class added

---
+++ Ruilwinkel/Article.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ruilwinkel
+{
+    public class Article
+    {
+        public int categoryid;
+        public string  
+    }
+}
