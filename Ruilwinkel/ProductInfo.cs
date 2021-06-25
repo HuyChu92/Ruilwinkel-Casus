0735c4277490e745853536eeca451ad8e0219cf9
LorenzoClermonts <lorenzoclermonts@ziggo.nl>
2021-06-25 10:13:36 +0200

    getallproducts api

---
+++ Ruilwinkel/ProductInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ruilwinkel
+{
+    public class ProductInfo
+    {
+        public int ID;
+
+        public int CategorieID;
+
+        public string ProductName;
+
+        public string Description;
+    }
+}
