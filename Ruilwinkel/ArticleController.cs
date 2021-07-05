85c9feb82cd9c640cf7fe88fc16d8e2049dd6486
Huy Chu <1135546chu@zuyd.nl>
2021-07-05 15:58:24 +0200

    final test

--- Ruilwinkel/ArticleController.cs
+++ Ruilwinkel/ArticleController.cs
@@ -76,49 +76,6 @@ namespace Ruilwinkel
             return "Article successfully inserted";
         }
 
-
-        /*
-        [HttpPost]
-        public void InsertNewArticle(Article article)
-        {
-            SqlConnection con = new SqlConnection();
-            con.ConnectionString = @"Data Source=productbeheerserver.database.windows.net;Initial Catalog=RuilwinkelDB;Persist Security Info=True;User ID=DevOps;Password=Zuyd2021";
-            con.Open();
-
-            SqlCommand cmd = new SqlCommand();
-            cmd.CommandText = "insert into ARTICLE values(@PRODUCTID, @COMMENTARY, @IMAGE)";
-            cmd.Parameters.AddWithValue("@PRODUCTID", article.productID);
-            cmd.Parameters.AddWithValue("@COMMENTARY", article.commantary);
-            cmd.Parameters.AddWithValue("@IMAGE", article.image);
-            cmd.Connection = con;
-            cmd.ExecuteNonQuery();
-            con.Close();
-        }
-        */
-
-        //[HttpGet]
-        //public List<int> GetAvailableArticles()
-        //{
-        //    List<int> articleStatus = new List<int>();
-
-        //    SqlConnection con = new SqlConnection();
-        //    con.ConnectionString = @"Data Source=productbeheerserver.database.windows.net;Initial Catalog=RuilwinkelDB;Persist Security Info=True;User ID=DevOps;Password=Zuyd2021";
-        //    con.Open();
-
-        //    SqlCommand cmd = new SqlCommand();
-        //    cmd.CommandText = "SELECT ARTICLE.ID, PRODUCT.PRODUCTNAME, PRODUCT.DESCRIPTION, CATEGORY.CATEGORYNAME, ARTICLE.STATUS, [USER].FIRSTNAME, [USER].LASTNAME, CATEGORY.POINTS FROM PRODUCT INNER JOIN ARTICLE ON PRODUCT.ID = ARTICLE.PRODUCTID INNER JOIN CATEGORY ON PRODUCT.CATEGORYID = CATEGORY.ID INNER JOIN[USER] ON ARTICLE.RENTERID = [USER].ID ";
-        //    cmd.Connection = con;
-        //    SqlDataReader dr = cmd.ExecuteReader();
-        //    while (dr.Read())
-        //    {
-        //        int articleID = int.Parse(dr.GetValue(0).ToString());
-        //        articleStatus.Add(articleID);
-        //    }
-        //    con.Close();
-
-        //    return articleStatus;
-
-        //}
-        
+  
     }
 }
