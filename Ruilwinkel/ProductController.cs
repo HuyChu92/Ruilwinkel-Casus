16f05d33de20c70b6a861bcf6736a454584cff31
Huy Chu <1135546chu@zuyd.nl>
2021-07-06 14:35:25 +0200

    test

--- Ruilwinkel/ProductController.cs
+++ Ruilwinkel/ProductController.cs
@@ -80,43 +80,6 @@ namespace Ruilwinkel
         //    con.Close();
         //}
 
-        /*
-        [HttpPost]
-        [Route("api/FileUploading/UploadFile")]
-        public async Task<string> UploadFile()
-        {
-            var ctx = HttpContext.Current;
-            var root = ctx.Server.MapPath("~/App_Data");
-            var provider = new MultipartFormDataStreamProvider(root);
-
-
-            try
-            {
-                await Request.Content
-                    .ReadAsMultipartAsync(provider);
-
-                foreach (var file in provider.FileData)
-                {
-                    var name = file.Headers
-                        .ContentDisposition
-                        .FileName;
-
-                    // remove double quotes from string.
-                    name = name.Trim('"');
-
-                    var localFileName = file.LocalFileName;
-                    var filePath = Path.Combine(root, name);
-
-                    File.Move(localFileName, filePath);
-                }
-            }
-            catch (Exception e)
-            {
-                return $"Error: {e.Message}";
-            }
-
-            return "File uploaded!";
-        }
-        */
+     
     }
 }
